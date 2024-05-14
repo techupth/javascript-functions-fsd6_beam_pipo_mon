25310fd279cb85c2cdffffecbfd662cd484615a7
Jakkaphob <jkpbeam@gmail.com>
2024-05-14 12:29:05 +0700

    feat: add Javvascript Function ex 1-6 as feature/beam

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,18 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+let addResult = calculator.add(10, 20);
+let divideResult = calculator.divide(3000, 10);
+
+console.log(calculator.add(10, 5));
+console.log(calculator.subtract(10, 5));
+console.log(calculator.multiply(10, 5));
+console.log(calculator.divide(10, 5));
+
+console.log(addResult);
+console.log(divideResult);
