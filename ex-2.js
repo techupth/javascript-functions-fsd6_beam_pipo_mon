ae36021db808725942dd2fc5e2bc0b817fa4da2f
Mon <tracer.coolkid@gmail.com>
2024-05-14 13:53:15 +0700

    ex1-6 js-functions

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,27 @@
 // Start coding here
+// Start coding here
+function add(a, b) {
+    return a + b;
+};
+function subtract(a, b) {
+    return a - b;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function divide(a, b) {
+    return a / b;
+};
+
+let add = function (a, b) {
+    return a + b;
+};
+let subtract = function (a, b) {
+    return a - b;
+};
+let multiply = function (a, b) {
+    return a * b;
+};
+let divide = function (a, b) {
+    return a / b;
+};
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,18 @@
 // Start coding here
+function add(a, b) {
+    return a + b;
+};
+function subtract(a, b) {
+    return a - b;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function divide(a, b) {
+    return a / b;
+};
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
